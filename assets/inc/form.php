403555e359c1c73dbecdec0b0803a358bf21945f
ViniciusOLucio <vinicius.olucio@gmail.com>
2024-07-22 17:09:25 -0300

    alteracoes

--- assets/inc/form.php
+++ assets/inc/form.php
@@ -1,109 +1,84 @@
-<section class="contato container sessao grid-2 ">
-  <!-- bloco1 -->
-  <div class="contato-txt">
-    <h2>
-      Levamos a assistência jurídica a sério.
-    </h2>
+<div class="contato-bloco">
+    <style>
+      .contato-info {
+        position: relative;
+        background-image: url('<?php echo get_template_directory_uri() ?>/assets/img/contato-bg.webp');
+      }
+    </style>
 
-    <div>
-      <ul class="contato-txt-lista">
-        <li class="contato-topico-txt">
-          <div class="contato-topico-img">
-            <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.52558 4.24351L4.51782 7.07031L19.5396 21.2203L22.5474 18.3935L7.52558 4.24351Z" fill="black" />
-              <path d="M12.0254 0.000301516L9.01978 2.8291L15.0327 8.4851L18.0384 5.6563L12.0254 0.000301516Z" fill="black" />
-              <path d="M3.00606 8.48801L0 11.3164L6.01211 16.9732L9.01817 14.1448L3.00606 8.48801Z" fill="black" />
-              <path d="M12.7552 20.0015H0.00146484V22.0015H12.7552V20.0015Z" fill="black" />
-            </svg>
-
-          </div>
-          <div class="contato-bloco-txt">
-            <h3>
-              Atendimento Personalizado
-            </h3>
-            <p>
-              Conte conosco para um atendimento único e personalizado. Estamos prontos para resolver questões previdenciárias
-            </p>
-          </div>
-        </li>
-        <!-- topico2 -->
-        <li class="contato-topico-txt">
-          <div class="contato-topico-img">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-              <rect y="0.00146484" width="24" height="24" fill="url(#pattern0)" />
-              <defs>
-                <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
-                  <use xlink:href="#image0_233_1601" transform="scale(0.0416667)" />
-                </pattern>
-                <image id="image0_233_1601" width="24" height="24" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAhUlEQVR4nO2USQ6AIAwA5+TD9SB+C+Up+I160cTEqqyaGCfpBZoOaQH4icABUihGTSCF48C20ZJOGyLQaIABmAEPmHUtqs6VwCgtMLkCtxuWVwReyYsSTIANEOzzogR3LepPcpOHbNZTZw8555p2IYJqD20sWNzyBvLUXyS1BNWRv0XfZAERFuHoOmGncwAAAABJRU5ErkJggg==" />
-              </defs>
-            </svg>
-
-          </div>
-          <div class="contato-bloco-txt">
-            <h3>
-              Especialização Jurídica Abrangente
-            </h3>
-            <p>
-              Informações sobre a ampla gama de áreas jurídicas em que atuamos, destacando nossa especialização.
-            </p>
-          </div>
-        </li>
-        <!-- topico3 -->
-        <li class="contato-topico-txt">
-          <div class="contato-topico-img">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/icon.svg" alt="Card Image">
-
-
-          </div>
-          <div class="contato-bloco-txt">
-            <h3>
-              Transparência nos Honorários
-            </h3>
-            <p>
-              Detalhes sobre nossa política de preços transparentes, visando fornecer clareza e honestidade
-            </p>
-          </div>
-        </li>
-      </ul>
+    <div class="contato-info bd-sm">
+      <div class="contato-info-txt">
+        <h2>
+          Informações de contato
+        </h2>
+        <p>
+          Como podemos ajudá-lo?
+        </p>
+      </div>
+      <div class="contato-info-loc">
+        <a href="nav-item" class="gotham">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Telefone Escritorio de Advocacia.svg" alt="">
+          <span>
+            (00)0 0000-0000
+          </span>
+        </a>
+        <a href="" class="gotham">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Email Escritorio de Advocacia.svg" alt="">
+          <span>
+            contato@contato.com
+          </span>
+        </a>
+        <a href="" class="gotham">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Localizacao Escritorio de Advocacia.svg" alt="">
+          <span>
+            Dr Paschoal Brando, 315 |SP
+          </span>
+        </a>
+      </div>
+      <div class="redes-sociais">
+        <a href="" class="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Instagram Gomes e Meyer Escritorio de Advocacia.svg" alt="">
+        </a>
+        <a href="" class="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Twitter Gomes e Meyer Escritorio de Advocacia.svg" alt="">
+        </a>
+        <a href="" class="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Linkedin Escritorio de Advocacia.svg" alt="">
+        </a>
+        <a href="" class="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Facebook Escritorio de Advocacia.svg" alt="">
+        </a>
+        <a href="" class="">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Whatsapp Escritorio de Advocacia.svg" alt="">
+        </a>
+      </div>
     </div>
+    <div class="contato-form">
 
-  </div>
-  <!-- bloco2 -->
-  <div class="contato-formulario">
-    <h2>
-      Obtenha um Orçamento para a Assistência Jurídica
-    </h2>
-
-    <div class="contact-form">
-      <?php echo do_shortcode('[contact-form-7 id="b0ffc70" title="formulario"]'); ?>
-      <!-- <form action="/enviar" method="post">
-
-        <div class="form-elemento">
-          <label for="nome">Nome completo</label>
-          <input type="text" id="nome" name="nome" required placeholder="Nome:">
+      <form action="/enviar-contato" method="post">
+        <div class="form-item">
+          <label for="nome">Nome:</label>
+          <input type="text" id="nome" name="nome" required placeholder="John Doe">
+          <hr>
         </div>
-
-        <div class="form-elemento">
+        <div class="form-item">
           <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required placeholder="Email:">
+          <input type="email" id="email" name="email" required placeholder="contato@contato.com">
+          <hr>
         </div>
-
-        <div class="form-elemento">
-          <label for="assunto">Assunto:</label>
-          <input type="text" id="assunto" name="assunto" required placeholder="Assunto:">
+        <div class="form-item">
+          <label for="telefone">Telefone:</label>
+          <input type="tel" id="telefone" name="telefone" placeholder="(19) 9 9384-3063">
+          <hr>
         </div>
-
-        <div class="form-elemento">
+        <div class="form-item">
           <label for="mensagem">Mensagem:</label>
-          <textarea id="mensagem" name="mensagem" rows="4" required placeholder="Mensagem:"></textarea>
+          <textarea id="mensagem" name="mensagem" placeholder="Escreva sua mensagem..." required></textarea>
+          <hr>
         </div>
 
-        <button type="submit" class="btn-form">Enviar</button>
-      </form> -->
+        <input type="submit" value="Enviar">
+      </form>
+
     </div>
-
-
-
   </div>
-</section>
--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-// Template Name:Areas Atuação
+// Template Name:Blog
 ?>
 <?php get_header(); ?>
 
-<h1 class="text-3xl font-bold underline">OLA MUNDO</h1>
-<hr>
-<h1 class="cirka">CIRKA</h1>
-<hr>
-<h1 class="gotham">GOTAHM</h1>
 <?php get_footer(); ?>
