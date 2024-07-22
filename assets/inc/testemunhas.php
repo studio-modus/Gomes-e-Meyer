05e068fbd686867dd00a89b7c3747b60ef59295d
ViniciusOLucio <vinicius.olucio@gmail.com>
2024-07-22 18:09:33 -0300

    refatoracao home

---
+++ assets/inc/testemunhas.php
@@ -0,0 +1,83 @@
+<section class="comentarios secao container">
+  <div class="titulo">
+    <h2>
+      Testemunhas
+    </h2>
+  </div>
+  <ul class="comentarios-cards grid-3">
+    <li class="comentario-card">
+      <div class="comentario-perfil">
+        <div class="img-perfil">
+          <img src="<?= get_template_directory_uri(); ?>/assets/img/adv.jpeg" alt="" srcset="">
+        </div>
+        <div class="txt-perfil">
+          <h3>
+            Mario Santos
+          </h3>
+          <h4>
+            Consultor de Negócios
+          </h4>
+        </div>
+      </div>
+      <!--  -->
+      <div class="comentario-txt">
+        <h4>
+          Profissionalismo e Competência Notáveis
+        </h4>
+        <p>
+          Os advogados deste escritório demonstraram um nível de profissionalismo e competência que me deixaram muito impressionado. Resolveram meu caso com eficiência e clareza.
+        </p>
+      </div>
+    </li>
+    <!-- dois -->
+    <li class="comentario-card">
+      <div class="comentario-perfil">
+        <div class="img-perfil">
+          <img src="<?= get_template_directory_uri(); ?>/assets/img/adv.jpeg" alt="" srcset="">
+        </div>
+        <div class="txt-perfil">
+          <h3>
+            Mario Santos
+          </h3>
+          <h4>
+            Consultor de Negócios
+          </h4>
+        </div>
+      </div>
+      <!--  -->
+      <div class="comentario-txt">
+        <h4>
+          Profissionalismo e Competência Notáveis
+        </h4>
+        <p>
+          Os advogados deste escritório demonstraram um nível de profissionalismo e competência que me deixaram muito impressionado. Resolveram meu caso com eficiência e clareza.
+        </p>
+      </div>
+    </li>
+    <!-- 3 -->
+    <li class="comentario-card">
+      <div class="comentario-perfil">
+        <div class="img-perfil">
+          <img src="<?= get_template_directory_uri(); ?>/assets/img/adv.jpeg" alt="" srcset="">
+        </div>
+        <div class="txt-perfil">
+          <h3>
+            Mario Santos
+          </h3>
+          <h4>
+            Consultor de Negócios
+          </h4>
+        </div>
+      </div>
+      <!--  -->
+      <div class="comentario-txt">
+        <h4>
+          Profissionalismo e Competência Notáveis
+        </h4>
+        <p>
+          Os advogados deste escritório demonstraram um nível de profissionalismo e competência que me deixaram muito impressionado. Resolveram meu caso com eficiência e clareza.
+        </p>
+      </div>
+    </li>
+  </ul>
+</section>
